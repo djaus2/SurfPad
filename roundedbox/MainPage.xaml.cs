38e4b990b59c7f5e5efcbf02d2c02670b423e370
David Jones <davidjones@sportronics.com.au>
2018-01-27 16:48:36 +1100

    First commit containing first attempt at creating rounded box

---
+++ roundedbox/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace roundedbox
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //public double Width_1 = 5;
+        public Thickness W1 = new Thickness(5);
+        uc.MyUserControl1[] buttons = new uc.MyUserControl1[10];
+        public MainPage()
+        {
+            Resources.Add("GridLength1", new GridLength((double)10));
+            Resources.Add("GridLength2", new GridLength((double)120));
+            Resources.Add("GridRound", new CornerRadius((int)20));
+            this.InitializeComponent();
+            buttons[0] = new uc.MyUserControl1()
+            {
+                Name = "fred",
+                Id = 0,
+                Text = "Qwerty",
+                Row = 3,
+                Col = 3,
+                ContainrerGrid=TheGrid
+            };
+            buttons[1] = new uc.MyUserControl1()
+            {
+                Name = "fred",
+                Id = 1,
+                Text = "Qwerty2",
+                Row = 7,
+                Col = 5,
+                ContainrerGrid = TheGrid
+            };
+
+            Brush red = new SolidColorBrush(Colors.Beige);
+
+            buttons[2] = new uc.MyUserControl1("azx", 5, 7, "Arc", TheGrid, red);
+
+        }
+
+        private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Border_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ roundedbox/MyUserControl1.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace roundedbox.uc
+{
+    public  partial class MyUserControl1 : UserControl
+    {
+        public string Text {
+            get { return TheText.Text; }
+            set { TheText.Text = value; }
+         }
+        public int Id { get; set; }
+        public MyUserControl1()
+        {
+            this.InitializeComponent();
+        }
+        public MyUserControl1(string name, int row, int col, string text, Grid containerGrid)
+        {
+            this.InitializeComponent();
+            Name = name;
+            Row = row;
+            Col = col;
+            Text = text;
+            ContainrerGrid = containerGrid;
+        }
+
+        public MyUserControl1(string name, int row, int col, string text, Grid containerGrid, Brush background)
+        {
+            this.InitializeComponent();
+            Name = name;
+            Row = row;
+            Col = col;
+            Text = text;
+            ContainrerGrid = containerGrid;
+            Border.Background = background;
+        }
+
+        public int Row { get; set; } = 0;
+        public int Col { get; set; } = 0;
+
+        public int RowSpan { get; set; } = 1;
+        public int ColSpan { get; set; } = 1;
+
+        public Grid ContainrerGrid
+        {
+            set
+            {
+                    value.Children.Add(this);
+                    Grid.SetColumn(this, Col);
+                    Grid.SetRow(this, Row);
+                    Grid.SetColumnSpan(this, ColSpan);
+                    Grid.SetRowSpan(this, RowSpan);
+            }
+        }
+
+        private void TheText_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
