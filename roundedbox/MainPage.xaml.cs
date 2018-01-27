1ebe382a7353db39140258f89bd2ed071dca6b43
David Jones <davidjones@sportronics.com.au>
2018-01-27 21:58:11 +1100

    Refined functionality leading to rudiments for remote app interactions.

--- roundedbox/MainPage.xaml.cs
+++ roundedbox/MainPage.xaml.cs
@@ -23,48 +23,93 @@ namespace roundedbox
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        const int DefaultCellWidth= 120;
+        const int DefaultCellHeight = 120;
+        const int DefaultCellSpacing = 10;
         //public double Width_1 = 5;
-        public Thickness W1 = new Thickness(5);
-        uc.MyUserControl1[] buttons = new uc.MyUserControl1[10];
+        //public Thickness W1 = new Thickness(5);
+        uc.MyUserControl1[][] buttons = new uc.MyUserControl1[0][];
         public MainPage()
         {
-            Resources.Add("GridLength1", new GridLength((double)10));
-            Resources.Add("GridLength2", new GridLength((double)120));
-            Resources.Add("GridRound", new CornerRadius((int)20));
+            //Resources.Add("GridLength1", new GridLength((double)10));
+            //Resources.Add("GridLength2", new GridLength((double)120));
+            //Resources.Add("GridRound", new CornerRadius((int)20));
             this.InitializeComponent();
-            buttons[0] = new uc.MyUserControl1()
-            {
-                Name = "fred",
-                Id = 0,
-                Text = "Qwerty",
-                Row = 3,
-                Col = 3,
-                ContainrerGrid=TheGrid
-            };
-            buttons[1] = new uc.MyUserControl1()
-            {
-                Name = "fred",
-                Id = 1,
-                Text = "Qwerty2",
-                Row = 7,
-                Col = 5,
-                ContainrerGrid = TheGrid
-            };
+            InitTheGrid(4,4);
+            //buttons[1][1] = new uc.MyUserControl1()
+            //{
+            //    Name = "fred",
+            //    Text = "Qwerty",
+            //    Row = 1,
+            //    Col = 1,
+            //    ContainrerGrid=TheGrid
+            //};
+            //buttons[3][3] = new uc.MyUserControl1()
+            //{
+            //    Name = "fred",
+            //    Text = "Qwerty2",
+            //    Row = 3,
+            //    Col = 3,
+            //    ContainrerGrid = TheGrid
+            //};
 
-            Brush red = new SolidColorBrush(Colors.Beige);
+            Brush red = new SolidColorBrush(Colors.Red);
+            AddMyUserControl1( 0, 0,"arc1", "First", red,123,50);
+            AddMyUserControl1(1, 2, "arc2", "Second",null, 124, 5);
+            AddMyUserControl1(3, 3, "The quick brown fox jumps over the lazy dog",  "Third");
 
-            buttons[2] = new uc.MyUserControl1("azx", 5, 7, "Arc", TheGrid, red);
-
+            uc.MyUserControl1.ButtonTapped += MainPage_ButtonTapped1;
         }
 
-        private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        private void AddMyUserControl1(int row, int col, string text, 
+            //Optional parameters:
+            //Name or Id should be unique
+            string name = "", Brush background = null, int id = -1, int cnrRad = -1)
         {
-
+            buttons[row][col] = new uc.MyUserControl1(row,col,text,TheGrid,name,background,id,cnrRad);
         }
 
-        private void Border_Tapped(object sender, TappedRoutedEventArgs e)
+        private void MainPage_ButtonTapped1(string sender, int args)
         {
-
+            string name = sender;
+            int id = args;
         }
+
+        //private void MainPage_ButtonTapped(object sender, TappedRoutedEventArgs e)
+        //{
+        //    if (sender is Button)
+        //    {
+        //        Button but = (Button)sender;
+        //        string name = but.Name;
+        //    }
+        //    //throw new NotImplementedException();
+        //}
+
+        public void InitTheGrid(int x, int y, int Height = DefaultCellHeight, int Width = DefaultCellWidth, int space = DefaultCellSpacing)
+        {
+            TheGrid.Children.Clear();
+            buttons = new uc.MyUserControl1[x][];
+            for (int i = 0; i<x;i++)
+            {
+                RowDefinition rd = new RowDefinition();
+                rd.Height = new GridLength((double)space);
+                TheGrid.RowDefinitions.Add(rd);
+                buttons[i] = new uc.MyUserControl1[y];
+                RowDefinition rd2 = new RowDefinition();
+                rd2.Height = new GridLength((double)Height);
+                TheGrid.RowDefinitions.Add(rd2);
+            }
+            for (int j = 0; j < y; j++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                cd.Width = new GridLength((double)space);
+                TheGrid.ColumnDefinitions.Add(cd);
+                ColumnDefinition cd2 = new ColumnDefinition();
+                cd2.Width = new GridLength((double)Width);
+                TheGrid.ColumnDefinitions.Add(cd2);
+            }
+        }
+
+
     }
 }
--- roundedbox/MyUserControl1.xaml.cs
+++ roundedbox/MyUserControl1.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -19,26 +20,80 @@ namespace roundedbox.uc
 {
     public  partial class MyUserControl1 : UserControl
     {
+        static readonly  Brush DefaultBackground =  new SolidColorBrush (Colors.Beige);
+        const int FlagForDefaultVal = -1;
+        const int CornerRadiusVal = 5;
+
+        public CornerRadius DefaultCornerRadius
+        {
+            get {
+                return  new CornerRadius(CornerRadiusVal);
+            }
+        }
         public string Text {
-            get { return TheText.Text; }
-            set { TheText.Text = value; }
+            set { TheText.Content =
+                    new TextBlock
+                    {
+                        Text = value,
+                        TextWrapping = TextWrapping.Wrap,
+                        TextAlignment = TextAlignment.Center,
+                    };
+            }
          }
         public int Id { get; set; }
         public MyUserControl1()
         {
             this.InitializeComponent();
+            TheText.Width = 100;
+            TheText.Height = 110;
         }
-        public MyUserControl1(string name, int row, int col, string text, Grid containerGrid)
-        {
-            this.InitializeComponent();
-            Name = name;
-            Row = row;
-            Col = col;
-            Text = text;
-            ContainrerGrid = containerGrid;
-        }
+        //public MyUserControl1(string name, int row, int col, string text, Grid containerGrid)
+        //{
+        //    this.InitializeComponent();
+        //    Name = name;
+        //    Row = row;
+        //    Col = col;
+        //    Text = text;
+        //    ContainrerGrid = containerGrid;
+        //    Id = containerGrid.ColumnDefinitions.Count * row + col;
+        //    TheText.Width = 100;
+        //    TheText.Height = 110;
+        //}
 
-        public MyUserControl1(string name, int row, int col, string text, Grid containerGrid, Brush background)
+        //public MyUserControl1(string name, int row, int col, string text, Grid containerGrid, Brush background)
+        //{
+        //    this.InitializeComponent();
+        //    Name = name;
+        //    Row = row;
+        //    Col = col;
+        //    Text = text;
+        //    ContainrerGrid = containerGrid;
+        //    Borderx.Background = background;
+        //    Id = (containerGrid.ColumnDefinitions.Count/2) * row + col;
+        //    TheText.Width = 100;
+        //    TheText.Height = 110;
+        //}
+
+        //public MyUserControl1(string name,  int row, int col, string text, Grid containerGrid, int id = -1 )
+        //{
+        //    this.InitializeComponent();
+        //    Name = name;
+        //    Row = row;
+        //    Col = col;
+        //    Text = text;
+        //    ContainrerGrid = containerGrid;
+        //    if (id != -1)
+        //        Id = id;
+        //    else
+        //        Id = (containerGrid.ColumnDefinitions.Count/2) * row + col;
+        //    TheText.Width = containerGrid.ColumnDefinitions[0].Width.Value;
+        //    TheText.Height = containerGrid.RowDefinitions[0].Height.Value;
+        //}
+
+        public MyUserControl1(int row, int col, string text, Grid containerGrid, 
+            //Optional parameters:
+            //Name or Id should be unique
+            string name="", Brush background = null, int id = FlagForDefaultVal, int cnrRad= FlagForDefaultVal)
         {
             this.InitializeComponent();
             Name = name;
@@ -46,7 +101,25 @@ namespace roundedbox.uc
             Col = col;
             Text = text;
             ContainrerGrid = containerGrid;
-            Border.Background = background;
+
+            if (cnrRad == FlagForDefaultVal)
+                Borderx.CornerRadius = DefaultCornerRadius;
+            else
+                Borderx.CornerRadius = new CornerRadius(cnrRad);
+
+            if (background == null)
+                Borderx.Background = DefaultBackground;
+            else
+                Borderx.Background = background;
+
+            if (id != FlagForDefaultVal)
+                Id = id;
+            else
+                Id = (containerGrid.ColumnDefinitions.Count/2) * row + col;
+
+            TheText.Width = containerGrid.ColumnDefinitions[1].Width.Value;
+            TheText.Height = containerGrid.RowDefinitions[1].Height.Value;
+
         }
 
         public int Row { get; set; } = 0;
@@ -60,16 +133,24 @@ namespace roundedbox.uc
             set
             {
                     value.Children.Add(this);
-                    Grid.SetColumn(this, Col);
-                    Grid.SetRow(this, Row);
+                    Grid.SetColumn(this, 2*Col+1);
+                    Grid.SetRow(this, 2*Row+1);
                     Grid.SetColumnSpan(this, ColSpan);
                     Grid.SetRowSpan(this, RowSpan);
             }
         }
 
+        public static event TypedEventHandler<string,int> ButtonTapped;
+
         private void TheText_Tapped(object sender, TappedRoutedEventArgs e)
         {
 
+            if (ButtonTapped != null)
+            {
+                EventArgs ev = new EventArgs();
+
+                ButtonTapped(this.Name, this.Id);
+            }
         }
     }
 }
