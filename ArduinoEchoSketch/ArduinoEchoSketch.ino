4c876cb6610cd5640370cbde9f8989b704c712a9
DJonesEmbedded <davidjones@sportronics.com.au>
2018-02-05 23:10:52 +1100

    Refinments to teh sketch

--- ArduinoEchoSketch/ArduinoEchoSketch.ino
+++ ArduinoEchoSketch/ArduinoEchoSketch.ino
@@ -3,143 +3,83 @@
  Created:	1/31/2018 11:27:21 PM
  Author:	DavidJones
 */
-
-//// the setup function runs once when you press reset or power the board
-//void setup() {
-//
-//}
-//
-//// the loop function runs over and over again until power down or reset
-//void loop() {
-//  
-//}
-
-
 #include <SoftwareSerial.h>
 
-SoftwareSerial bt(2, 3); // RX, TX
+SoftwareSerial bt(2, 3); // RX, TX Pins
 
 char  thisByte;
-bool useName = true;
-enum Mode { ACK0, ACK1, ACK2, Running, GetString };
-Mode mode = ACK1;
-byte bytz;
+enum Mode { ACK0, ACK1, ACK2,ACK4, Running, GetJson, GetString };
+Mode mode = ACK0;
 
 void setup() {
 	bt.begin(9600);
 	Serial.begin(9600);
 	Serial.print("Started");
 	mode = ACK0;
-	useName = false;
 }
 
 void loop() {
 	bt.listen();
 	thisByte = bt.read();
-	if (useName)
+	// Read BT Serial char by char
+	// At start read messages to sync
+	// Expect '0'
+	// Send '1' as ack
+	// Expect '2'
+	// Send '3' as ack
+	// Expect '4'
+	// Send '5 as ack
+	// If char = '!' 
+	//   Send back '/' as ack ready to send json
+	//   Expect '/'
+	//   Send first json string (Config)
+	//   Expect '~'
+	//   Send back single line json string (MainMenu)
+	// Default: Just echo the character back
+	if (thisByte != -1)
 	{
-		// At start read messages as ending with last character #
-		// Expect ACK0#
-		// Send ACK1
-		// Expect ACK1
-		// Send Json config file
-		// Expect ACK12#
-		// Switch to single char mode
-		String msg = "";
-		if (thisByte != -1)
+		//Each char is interpretted as byte representing a keypress
+		//The byte is the id of button pressed + ' ' (so are printable
+		//bt.print(thisByte);
+		//bt.print('#');
+		switch (thisByte)
 		{
-			//Uses # terminated string
-			msg += thisByte;
-
-			while (thisByte != '#')
-			{
-				thisByte = bt.read();
-				if (thisByte != -1)
-					msg += thisByte;
-			}
-			//bt.write(thisByte);
-			int j = 0;
-			Serial.println(msg);
-			//bt.print(msg);
-			//if (mode == ACK0)
-			//{
-				if (msg == "ACK0#")
-				{
-					mode == ACK1;
-					//Send Json file
-					bt.print("ACK1#");
-				}
-			//}
-			///else if (mode == ACK1)
-			//{
-				else if (msg == "ACK2#")
-				{
-					mode == ACK2;
-					//Send Json file
-					bt.print("[{ \"ElementConfig\": \"Config\" },{ \"MainMenu\": [ \"Setup BT\", \"Setup Serial\", \"Show sensor list\", \"Back to sensor list\", \" = Sensor\" ]}]#");
-					bt.print("ACK3");
-				}
-			//}
-			//else if (mode == ACK2)
-			//{
-				else if (msg == "ACK4#")
-				{
-					mode == Running;
-					useName = false;
-				}
-			//}
-		}
-	}
-	else
-	{
-		if (thisByte != -1)
-		{
-				//Each char is interpretted as byte representing a keypress
-				//The byte is the id of button pressed + ' ' (so are printable
-				//bt.print(thisByte);
-				//bt.print('#');
-			switch (thisByte)
-			{
-			case '0':
-				bt.print('1');
-				break;
-			case '2':
-				bt.print('3');
-				break;
-			case '4':
-				bt.print('5');
-				break;
-			case '!': //Get Exclamation mark as indicator of request for Json
-				bytz = 200;
-				bt.print('/'); //Send back / meaning it will follow
-				break;
-			case '/':  //App sends this back
-				bt.print("{\"ElementConfig\":[ [ { \"iWidth\": 120 },{ \"iHeight\": 100 },{ \"iSpace\": 5 },{ \"iCornerRadius\": 10 },{ \"iRows\": 2 },{ \"iColumns\": 5 },{ \"sComPortId\": \"\\\\\\\\?\\\\USB#VID_26BA&PID_0003#5543830353935161A112#{86e0d1e0-8089-11d0-9ce4-08003e301f73}\" },{ \"sFTDIComPortId\": \"\\\\\\\\?\\\\FTDIBUS#VID_0403+PID_6001+FTG71BUIA#0000#{86e0d1e0-8089-11d0-9ce4-08003e301f73}\" },{ \"iComportConnectDeviceNo\": -1 },{ \"iFTDIComportConnectDeviceNo\": 1 },{ \"sUseSerial\": \"BT\" } ] ] }~");
-				break;
-			case '~':  //Then when it gets above then sends this back as confirmation
-				//bt.print("Hello World~");
-				bt.print("{\"MainMenu\":[ [ \"Setup BT\", \"Setup Serial\", \"Redo\", \"Show full list\", \"The quick brown fox jumps over the lazy dog\" ],[ \"First\", \"Back\", \"Next\", \"Last\", \"Show All\" ] ] }~");
-				mode = Running;
-				break;
-			default:
+		case '0':
+			bt.print('1');
+			mode = ACK1;
+			break;
+		case '2':
+			bt.print('3');
+			mode = ACK2;
+			break;
+		case '4':
+			bt.print('5');
+			mode = ACK4;
+			break;
+		case '!': //Get Exclamation mark as indicator of request for Json
+			bt.print('/'); //Send back / meaning it will follow
+			mode = GetJson;
+			break;
+		default:
+			if (mode==Running)
 				bt.print(thisByte);
-				break;
+			else if (mode = GetJson)
+			{
+				switch (thisByte)
+				{
+					case '/':  //App sends this back
+						bt.print("{\"Config\":[ [ { \"iWidth\": 120 },{ \"iHeight\": 100 },{ \"iSpace\": 5 },{ \"iCornerRadius\": 10 },{ \"iRows\": 2 },{ \"iColumns\": 5 },{ \"sComPortId\": \"\\\\\\\\?\\\\USB#VID_26BA&PID_0003#5543830353935161A112#{86e0d1e0-8089-11d0-9ce4-08003e301f73}\" },{ \"sFTDIComPortId\": \"\\\\\\\\?\\\\FTDIBUS#VID_0403+PID_6001+FTG71BUIA#0000#{86e0d1e0-8089-11d0-9ce4-08003e301f73}\" },{ \"iComportConnectDeviceNo\": -1 },{ \"iFTDIComportConnectDeviceNo\": 1 },{ \"sUseSerial\": \"BT\" } ] ] }~");
+						break;
+					case '~':  //Then when it gets above then sends this back as confirmation
+							   //bt.print("Hello World~");
+						bt.print("{\"MainMenu\":[ [ \"Setup BT\", \"Setup Serial\", \"Redo\", \"Show full list\", \"The quick brown fox jumps over the lazy dog\" ],[ \"First\", \"Back\", \"Next\", \"Last\", \"Show All\" ] ] }~");
+						mode = Running;
+						break;
+					default:
+						mode = Running;
+				}
 			}
+			break;
 		}
 	}
-	//  bt.write(thisByte);
-	//
-	//  bt.print(", dec: ");
-	//  bt.print(thisByte);
-	//  bt.print(", hex: ");
-	//  bt.print(thisByte, HEX);
-	//  bt.print(", oct: ");
-	//  bt.print(thisByte, OCT);
-	//  bt.print(", bin: ");
-	//  bt.println(thisByte, BIN);
-	//
-	//  if(thisByte == 126) {
-	//    thisByte = 32;
-	//  }
-	//  thisByte++;
 }
