9dfea8566a746d450d1ae414528efa326fb1e8ed
David Jones <davidjones@sportronics.com.au>
2018-02-24 00:35:59 +1100

    Some renaming of projects.

--- ArduinoEchoSketch/__vm/.ArduinoEchoSketch.vsarduino.h
+++ ArduinoEchoSketch/__vm/.ArduinoEchoSketch.vsarduino.h
@@ -77,7 +77,7 @@ typedef void *__builtin_va_list;
 #define pgm_read_float(address_short) float()
 #define pgm_read_ptr(address_short)   short()
 
-#include "ArduinoEchoSketch.ino"
+#include "ArduinoRemote.ino"
 #include "BTSerial.ino"
 #include "ServerSocket.ino"
 #include "USBSerial.ino"
